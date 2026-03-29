ee2186a6e1280f6d92205eed96bcd49705b57e39
lchen <chenlitchian@gmail.com>
2026-03-29 15:23:43 +0800

    feat: add android, google maps and leaflet

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { GoogleMap } from '@capacitor/google-maps';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent], //ExploreContainerComponent
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Tab1Page {
   constructor() {}
+
+  async ngOnInit() {
+    const map = await GoogleMap.create({
+      id: 'my-map',
+      element: document.getElementById('map')!,
+      apiKey: 'AIzaSyA6Dv7OwhSFMeAOjj2PLdk8AF5ixyPzEHg',
+      config: {
+        center: {
+          lat: 1.3216502,
+          lng: 103.8582341
+        },
+        zoom: 15
+      }
+    });
+  }
+
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  @ViewChild('mapContainer', { read: ElementRef }) mapContainer!: ElementRef;
+  private map: L.Map | null = null;
 
   constructor() {}
+
+  ngOnInit() {
+    this.initializeMap();
+  }
+
+  private initializeMap() {
+    // Wait for the view to be initialized before creating the map
+    setTimeout(() => {
+      if (this.mapContainer && this.mapContainer.nativeElement) {
+        this.map = L.map(this.mapContainer.nativeElement).setView([1.3216502, 103.8582341], 15);
+
+        // Add OpenStreetMap tiles
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '© OpenStreetMap contributors',
+          maxZoom: 19
+        }).addTo(this.map);
+
+        // Add a marker
+        L.marker([1.3216502, 103.8582341])
+          .addTo(this.map)
+          .bindPopup('Singapore')
+          .openPopup();
+      }
+    }, 100);
+  }
 
 }
